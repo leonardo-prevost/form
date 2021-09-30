7a9d1dc583cdeb2dbad2d029df0005a38b0ccd36
Leonardo Prevost <prevostleonardo@gmail.com>
2021-09-30 14:42:21 -0300

    defining validation rules

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-let form = document.querySelector('.validator')
+let validator = {
+    handleSubmit:(e)=>{
+        e.preventDefault();
+
+        let send = true;
+
+        let inputs = form.querySelectorAll('input');
+
+        for(let i=0;i<inputs.length;i++){
+            let input = inputs[i];
+            console.log(input);
+        }
+        send =false;
+        if(send){
+            form.submit();
+        }
+    }
+};
+
+let form = document.querySelector('.validator');
+form.addEventListener('submit', validator.handleSubmit);
